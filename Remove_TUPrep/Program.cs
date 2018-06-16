8b4986dc2273fb18af24a10df0700a7a9bee70b9
Ilya Nikolaev <nikolaeffredbull@gmail.com>
2018-06-16 16:38:33 +0300

    16.06.2018

---
+++ Remove_TUPrep/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Remove_TUPrep
+{
+    class Program
+    {
+
+        private static string _fileName = "Druzhba.txt";
+
+        // Для удаления
+        private static List<string> _luNames = new List<string>()
+        {
+            "LU_NIKOL1",
+            "LU_KLIN",
+            "LU_KIJEV",
+            "LU_SOSED",
+            "LU_KUZN"
+        };
+
+        static void Main()
+        {
+            int countRemoved = 0;
+            int countAllBefore = 0;
+            int countAllAfter = 0;
+            int countAdded = 0;
+
+            var outputFile = new StreamWriter("reworked_" + _fileName, false);
+
+            // Удаление TUPrep
+            var allLines = File.ReadAllLines(_fileName);
+            foreach (var line in allLines)
+            {
+                string[] lineParts = line.Split('\t');
+                if (!String.IsNullOrEmpty(lineParts[8]))
+                    countAllBefore++;
+                if (lineParts[8].ToUpper().Contains("TUPREP") && (lineParts[8].ToUpper().Contains(_luNames[0])
+                                                                  || lineParts[8].ToUpper().Contains(_luNames[1])
+                                                                  || lineParts[8].ToUpper().Contains(_luNames[2])
+                                                                  || lineParts[8].ToUpper().Contains(_luNames[3])
+                                                                  || lineParts[8].ToUpper().Contains(_luNames[4]))
+                                                              && !lineParts[8].ToUpper().Contains("NPS_"))
+                {
+                    lineParts[8] = string.Empty;
+                    countRemoved++;
+                }
+
+                foreach (var linePart in lineParts)
+                {
+                    outputFile.Write(linePart + "\t");
+                }
+
+                outputFile.WriteLine();
+            }
+            outputFile.Close();
+            //Console.WriteLine($"Removed: {countRemoved} from {countAll} tags");
+
+            // Добавление TUPrep ко всем остальным
+            var reworkedLines = File.ReadAllLines("reworked_" + _fileName);
+            var outputFile2 = new StreamWriter("druzhba_good.txt", false);
+            foreach (var line in reworkedLines)
+            {
+                string[] lineParts = line.Split('\t');
+                // Вытаскиваем тег на задвижку
+                string tagName = string.Empty;
+                if (lineParts[3].Equals("Valve") && (!lineParts[1].Contains("МНС") || !lineParts[1].Contains("УРД") ||
+                                                     !lineParts[1].Contains("НПС") || !lineParts[1].Contains("РП")) ||
+                    !lineParts[1].Contains("ПНС") && lineParts[6].Equals("ID") && !string.IsNullOrEmpty(lineParts[8]))
+                {
+                    tagName = lineParts[8];
+                }
+                else
+                {
+                    tagName = string.Empty;
+                }
+                // Добавляем тег TUPREP на текущее задвижку, если она отвечает условиям отсутствия ЦСПА
+                if (!string.IsNullOrEmpty(tagName) && (!lineParts[8].ToUpper().Contains(_luNames[0])
+                    || !lineParts[8].ToUpper().Contains(_luNames[1])
+                    || !lineParts[8].ToUpper().Contains(_luNames[2])
+                    || !lineParts[8].ToUpper().Contains(_luNames[3])
+                    || !lineParts[8].ToUpper().Contains(_luNames[4]))
+                    && !lineParts[8].ToUpper().Contains("NPS_") && (lineParts[6].Equals("CanBeControlled")))
+                {
+                    lineParts[8] = tagName + ".TUPREP";
+                    countAdded++;
+                }
+
+
+                foreach (var linePart in lineParts)
+                {
+                    outputFile2.Write(linePart + "\t");
+                }
+                outputFile2.WriteLine();
+            }
+
+            outputFile2.Close();
+
+            Console.WriteLine($"Total tags count before operations: {countAllBefore}\nTags removed: {countRemoved} added: {countAdded}");
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
